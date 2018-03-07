1ad26ba46e5a76d7ac376469c5c5c2835247e537
Tony Lauf <LAU5619@s258769.nml.com>
2018-03-07 11:25:50 -0600

    no message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      username: '',
+      email: '',
+      avatar: ''
+    }
+
+  }
+  componentDidMount(){
+    axios.get('http://5a747e5f61c2a40012894ab8.mockapi.io/msgbrd/v1/users')
+    .then(({data:users}) => {
+      this.setState({users})
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +29,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <form>
+              <input type="text" placeholder="email" />
+          </form>
+            {this.state.users.map((user) => (
+              <p key={user.id}>{user.username}</p>
+            ))}
       </div>
     );
   }
